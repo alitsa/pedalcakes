3f6430b883f1d1fb235436694260983fd37e82e9
Walnut Kylie <amalia.litsa@gmail.com>
2024-01-19 16:49:43 -0600

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Title of the document</title>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rubik+Mono+One&display=swap" rel="stylesheet">
+
+<link rel="stylesheet" href="styles/cake.css" />
+</head>
+
+
+<body>
+
+<article>
+  <div class="flex">
+    <div>
+      <h1>Pedal Cakes</h1>
+      <p>
+        Meow meow meow meow meow meow meow meow meow meow meow meow
+      </p>
+
+<figure>
+<img src="https://i.giphy.com/3oEjHFHaBKjH2lojSg.webp" alt="" />
+</figure>
+
+    </div>
+  </div>
+</article>
+
+</body>
+
+</html>
